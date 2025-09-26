61fe172eacce7068f6575013d530fd7fbd70bf82
Chinmoy Kalita <chinmoykalita210@gmail.com>
2025-09-26 19:34:45 +0530

    churn pundit policy update

--- app/policies/churn_analytics_policy.rb
+++ app/policies/churn_analytics_policy.rb
@@ -3,17 +3,17 @@
 class ChurnAnalyticsPolicy < ApplicationPolicy
   def index?
     Feature.active?(:churn_analytics_enabled, seller) &&
-      seller_has_subscription_products? &&
-      (
-        user.role_admin_for?(seller) ||
-        user.role_marketing_for?(seller) ||
-        user.role_support_for?(seller) ||
-        user.role_accountant_for?(seller)
-      )
+    (
+      user.role_admin_for?(seller) ||
+      user.role_marketing_for?(seller) ||
+      user.role_support_for?(seller) ||
+      user.role_accountant_for?(seller)
+    ) &&
+      seller_has_subscription_products?
   end
 
   private
     def seller_has_subscription_products?
-      seller.products_for_creator_analytics.any? { it.is_recurring_billing? || it.is_tiered_membership? }
+      CreatorAnalytics::Churn.new(seller: seller).subscription_products.any?
     end
 end
