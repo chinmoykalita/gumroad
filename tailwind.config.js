eaf71cfb255b56c08da98dc430956d970e2e9da9
Chinmoy Kalita <chinmoykalita210@gmail.com>
2025-10-09 22:47:53 +0530

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,7 +73,6 @@ export default {
         background: "rgb(var(--filled))",
         foreground: "rgb(var(--color))",
         border: "rgb(var(--color) / var(--border-alpha))",
-        accent: "rgb(var(--accent))",
       },
       boxShadow: {
         DEFAULT: "0.25rem 0.25rem 0 currentColor",
